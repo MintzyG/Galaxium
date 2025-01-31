904eea890d4c2dc15c9106309dd81b9b7cf7eeeb
MintzyG <ericbraga2000@gmail.com>
2025-01-30 23:34:39 -0300

    Removed unused pkgs

--- users/sophia/default.nix
+++ users/sophia/default.nix
@@ -4,18 +4,15 @@
   programs.home-manager.enable = true;
 
   imports = [
-    ../../shared/user/apps/firefox.nix
     ../../shared/user/apps/mpd.nix
     ../../shared/user/development/c.nix
     ../../shared/user/development/git.nix
     ../../shared/user/development/go.nix
     ../../shared/user/development/lazygit.nix
     ../../shared/user/development/nvim.nix
-    ../../shared/user/development/ruby.nix
     ../../shared/user/shell/fish.nix
     ../../shared/user/services/ags.nix
     ../../shared/user/services/swww.nix
-    ../../shared/user/services/picom.nix
     ../../shared/user/shell/starship.nix
     ../../shared/user/utilities/eza.nix
     ../../shared/user/utilities/yazi.nix
@@ -34,7 +31,6 @@
     packages = with pkgs; [ 
       inputs.zen-browser.packages."x86_64-linux".twilight
       brightnessctl
-      chromium
       discord
       gimp
       godot_4
@@ -43,7 +39,6 @@
       osu-lazer-bin
       postman
       spotify
-      texliveFull
       unzip
     ];
   };
